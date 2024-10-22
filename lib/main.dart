4a080977d0345071d38a85d02b6c4a4535927cc7
far1h <farihmuhammad45@gmail.com>
2024-10-22 08:40:50 +0700

    Configure Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 136, 94, 207),
       body: Center(child: Text("Hello World!")),
     ),
   ));
