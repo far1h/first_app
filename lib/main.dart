54e8b8221d7f559a8a81fdca49e6cb6b66f95f93
far1h <farihmuhammad45@gmail.com>
2024-10-22 09:03:14 +0700

    Build Custom Components

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 57, 40, 85),
-            Color.fromARGB(255, 81, 18, 184)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-          child: const Center(
-              child: Text(
-            "Hello World!",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-            ),
-          ))),
+      body: GradientContainer(),
     ),
   ));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 57, 40, 85),
+          Color.fromARGB(255, 81, 18, 184)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child: const Center(
+            child: Text(
+          "Hello World!",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        )));
+    throw UnimplementedError();
+  }
 }
