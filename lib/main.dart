e725985a208d25b9cc94cf2efacae185c2e59b1d
far1h <farihmuhammad45@gmail.com>
2024-10-22 08:52:08 +0700

    Configure Widgets & Objects using Auto Complete

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ void main() {
                Color.fromARGB(255, 57, 40, 85),
                Color.fromARGB(255, 81, 18, 184)
             ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
           )),
           child: const Center(child: Text("Hello World!"))),
     ),
