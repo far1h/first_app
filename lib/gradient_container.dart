f624f2a5dbccac6c2e369b27150db177d9258e74
far1h <farihmuhammad45@gmail.com>
2024-10-22 23:34:41 +0700

    List of Colors: Create Reusable Widgets & Constructor Functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,17 +5,15 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
 
+    final List<Color> colors;
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 57, 40, 85),
-          Color.fromARGB(255, 81, 18, 184)
-        ], begin: startAlignment, end: endAlignment)),
-        child: const Center(child: StyledText()));
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: colors, begin: startAlignment, end: endAlignment)),
+        child: const Center(child: StyledText("Hello World")));
     throw UnimplementedError();
   }
 }
