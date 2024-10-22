3ebcf6f475f27404380f1aa1a3467894f3f2eb41
far1h <farihmuhammad45@gmail.com>
2024-10-22 23:15:03 +0700

    Try variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,21 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
           Color.fromARGB(255, 57, 40, 85),
           Color.fromARGB(255, 81, 18, 184)
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-        child: const Center(
-            child: StyledText()));
+        ], begin: startAlignment, end: endAlignment)),
+        child: const Center(child: StyledText()));
     throw UnimplementedError();
   }
 }
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -8,11 +8,11 @@ class StyledText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Text(
-              "Hello World!",
-              style: TextStyle(
-    color: Colors.white,
-    fontSize: 28,
-              ),
-            );
+      "Hello World!",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+      ),
+    );
   }
 }
