fe4cf68ad543c060fd01692cf831ce0ddbdf1204
far1h <farihmuhammad45@gmail.com>
2024-10-22 08:54:54 +0700

    Style Text

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,18 @@ void main() {
     home: Scaffold(
       body: Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            colors: [
-               Color.fromARGB(255, 57, 40, 85),
-               Color.fromARGB(255, 81, 18, 184)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-          )),
-          child: const Center(child: Text("Hello World!"))),
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 57, 40, 85),
+            Color.fromARGB(255, 81, 18, 184)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: const Center(
+              child: Text(
+            "Hello World!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            ),
+          ))),
     ),
   ));
 }
