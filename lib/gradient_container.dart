71b761a000b468ae956488f02eafb344fb2db12d
far1h <farihmuhammad45@gmail.com>
2024-10-22 23:47:33 +0700

    Add Multiple Constructor Functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -7,14 +7,21 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
-    final Color color1;
-    final Color color2;
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [color1,color2], begin: startAlignment, end: endAlignment)),
-        child: const Center(child: StyledText("Hello World")));
+            gradient: LinearGradient(
+                colors: [color1, color2],
+                begin: startAlignment,
+                end: endAlignment)),
+        child: const Center(child: Image.asset(name)));
     throw UnimplementedError();
   }
 }
