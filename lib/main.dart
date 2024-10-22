a4714eee9acc897b1a5463d059223aee91581430
far1h <farihmuhammad45@gmail.com>
2024-10-22 08:50:19 +0700

    Add Gradient Colors using Lists

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: 
-          )
-        ),
-        child: const Center(child: Text("Hello World!"))),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+               Color.fromARGB(255, 57, 40, 85),
+               Color.fromARGB(255, 81, 18, 184)
+            ],
+          )),
+          child: const Center(child: Text("Hello World!"))),
     ),
   ));
 }
